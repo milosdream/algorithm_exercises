c5de1168a0e589accfb4685b5402d95392eb8250
App Academy Student <student@appacademy.io>
2015-11-05 11:06:16 -0800

    phase 1 done of largest sub sum

--- execution_time_differences.rb
+++ execution_time_differences.rb
@@ -1,12 +1,30 @@
 def my_min(array)
-  smallest = nil
+  smallest = array.first
 
-  0.upto(array.length-1) do |i|
-    0.upto(array.length-1) do |j|
-      break if array[j] < array[i]
-      smallest = array[i]
+  array.each do |x|
+    if x < smallest
+      smallest = x
     end
   end
 
   smallest
+end
+
+
+def largest_sub_sum(array)
+  sub_arrays = []
+
+  0.upto(array.length - 1) do |i|
+    i.upto(array.length - 1) do |j|
+      sub_arrays << array[i..j]
+    end
+  end
+
+  max = sub_arrays[0].inject(:+)
+  sub_arrays.each do |sub_array|
+    current_sum = sub_array.inject(:+)
+    max = current_sum if current_sum > max
+  end
+
+  max
 end
